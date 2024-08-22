ab201af885226fe8eb3f5ff5009b349a114ebf10
Pedro Alexis Gonzalez Espinoza <pgonzaleze@vortex.corp>
2024-08-22 15:16:19 -0600

    cambie el front para usar adminlte, el anterior estaba mal maquetado

--- src/views/js/apiLogInStatus.js
+++
@@ -1,7 +0,0 @@
-
-function simulateLoginStatus(isLoggedIn) {
-    return new Promise((resolve) => {
-        // Simulamos una respuesta de API con el valor proporcionado
-        resolve({ loggedIn: isLoggedIn });
-    });
-}
---
+++ src/views/js/metadata.js
@@ -0,0 +1,8 @@
+// metadata.js
+const metadata = {
+    author: "Desarrollo Web - Megacable 2024",
+    description: "Panel de control para megacable",
+    viewport: "width=device-width, initial-scale=1.0",
+    title: "CPanel Mega",
+    keywords:"NonKeywords"
+  };
