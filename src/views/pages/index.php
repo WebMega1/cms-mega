8e397cdabb3abc96262b56f98218308ed1683a35
Pedro Alexis Gonzalez Espinoza <pgonzaleze@vortex.corp>
2024-08-29 15:13:50 -0600

    cambios en visual y agregamos funcionalidad php temporal

--- src/views/pages/index.php
+++ src/views/pages/index.php
@@ -23,7 +23,7 @@
                             <ol class="breadcrumb float-sm-end">
                                 <li class="breadcrumb-item"><a href="#">Home</a></li>
                                 <li class="breadcrumb-item active" aria-current="page">
-                                    Dashboard
+                                    Dashboard 
                                 </li>
                             </ol>
                         </div>
